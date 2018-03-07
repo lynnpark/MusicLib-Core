6791e5847516a71df4ec6003641265848198e940
LynnPark <interfectus13@gmail.com>
2018-03-07 17:09:34 -0500

    Created Artists Model. Testing Git.

--- MusicLib/Models/Artist.cs
+++ MusicLib/Models/Artist.cs
@@ -7,5 +7,13 @@ namespace MusicLib.Models
 {
     public class Artist
     {
+        public int Id { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
     }
 }
